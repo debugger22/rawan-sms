009f713c1d2907cc9f6efd1267671610a230e59a
Sudhanshu Mishra <mrsud94@gmail.com>
2014-06-22 00:04:42 +0530

    Add LICENSE

--- smsapp/views.py
+++ smsapp/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import auth
-from django.views.decorators.cache import cache_page
-from django.core.cache import cache
 from django.contrib.auth.decorators import login_required
 
 from coresms.models import *
